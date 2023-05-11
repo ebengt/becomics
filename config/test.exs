d7b2408fc57f40c23cbd049846913bdb4cfd3999
Bengt Kleberg <bengt.kleberg@travelping.com>
2023-05-11 07:55:40 +0200

    Update dependencies

--- config/test.exs
+++ config/test.exs
@@ -9,7 +9,7 @@ config :becomics, Becomics.Repo,
   username: "postgres",
   password: "postgres",
   hostname: System.get_env("POSTGRES_HOST") || "localhost",
-  port: String.to_integer(System.get_env("POSTGRES_PORT") || "5433"),
+  port: String.to_integer(System.get_env("POSTGRES_PORT") || "5432"),
   database: "becomics_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
--- mix.exs
+++ mix.exs
@@ -5,9 +5,9 @@ defmodule Becomics.MixProject do
     [
       app: :becomics,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:gettext] ++ Mix.compilers(),
+      compilers: Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
@@ -33,21 +33,24 @@ defmodule Becomics.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps() do
     [
-      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:phoenix, "~> 1.6.7"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:phoenix, "~> 1.7"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 3.0"},
+      {:phoenix_html, "~> 3.3"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.17.5"},
+      {:phoenix_live_view, "~> 0.18.16"},
+      {:phoenix_view, "~> 2.0"},
+      {:phoenix_live_dashboard, "~> 0.7.2"},
+      {:esbuild, "~> 0.7", runtime: Mix.env() == :dev},
       {:floki, ">= 0.30.0", only: :test},
-      {:phoenix_live_dashboard, "~> 0.6"},
-      {:esbuild, "~> 0.4", runtime: Mix.env() == :dev},
+      {:tailwind, "~> 0.2.0", runtime: Mix.env() == :dev},
       {:swoosh, "~> 1.3"},
+      {:finch, "~> 0.13"},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
-      {:gettext, "~> 0.18"},
+      {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"}
     ]
