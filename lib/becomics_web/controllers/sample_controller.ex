45a2c1f5b0ea145c6dcda56be96a261ea78e5323
bengt kleberg <cean.ebengt@gmail.com>
2023-07-20 13:13:10 +0200

    Better sample2 (#11)
    * Better sample for monthly reading

--- lib/becomics_web/controllers/sample_controller.ex
+++ lib/becomics_web/controllers/sample_controller.ex
@@ -17,15 +17,39 @@ defmodule BecomicsWeb.SampleController do
     select(comics, days_in_month, zero_based, overlap)
   end
 
-  @doc """
-  select a subset given how many such subsets there should be (days) and which of the subsets
-  it is.
-  Overlap 1 so the user will see continuity (last item yesterday is todays first item).
-  """
-  def select(comics, days, which, overlap) do
-    sample_size = Enum.count(comics) / days
-    Enum.slice(comics, int(sample_size * which), int(sample_size + overlap))
+  #
+  # Internal functions
+  #
+
+  defp select([], _total_selection, _which, _overlap), do: []
+
+  defp select(comics, total_selection, which, overlap) do
+    samples_per_day = select_samples(comics, total_selection)
+    select_loop(comics, samples_per_day, which, overlap)
   end
 
-  defp int(f), do: Kernel.trunc(Float.round(f))
+  defp select_loop(comics, [sample | _], 0, overlap), do: Enum.take(comics, sample + overlap)
+
+  defp select_loop(comics, [sample | t], count_down, overlap),
+    do: select_loop(Enum.drop(comics, sample), t, count_down - 1, overlap)
+
+  defp select_samples(comics, total_selection) do
+    amount = Enum.count(comics)
+    low_per_day = Kernel.div(amount, total_selection)
+    high_per_day = low_per_day + 1
+    low_selection = low_per_day * total_selection
+    days_with_high_selection = amount - low_selection
+
+    List.duplicate(high_per_day, days_with_high_selection) ++
+      List.duplicate(low_per_day, total_selection - days_with_high_selection)
+  end
+
+  if Mix.env() === :test do
+    def test_select(comics, total_selection, which), do: select(comics, total_selection, which, 0)
+
+    def test_select(comics, total_selection, which, overlap),
+      do: select(comics, total_selection, which, overlap)
+
+    def test_select_samples(comics, total_selection), do: select_samples(comics, total_selection)
+  end
 end
