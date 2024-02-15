adbbfe2be41a5424b66427ef2fd7bca9d09aa61c
Bengt Kleberg <bengt.kleberg@travelping.com>
2024-02-15 20:23:43 +0100

    Format

--- lib/becomics_web/controllers/like_controller.ex
+++ lib/becomics_web/controllers/like_controller.ex
@@ -3,8 +3,12 @@ defmodule BecomicsWeb.LikeController do
     formats: [:html]
 
   def like(conn, %{"like" => name}) do
-    s = name |> sane_name |> wildcard_join
-    comics = Becomics.comic_like(s) |> BecomicsWeb.ControllersLib.prepare_to_render_form()
+    comics =
+      name
+      |> sane_name()
+      |> wildcard_join()
+      |> Becomics.comic_like()
+      |> BecomicsWeb.ControllersLib.prepare_to_render_form()
 
     render(conn, :like, comics: comics)
   end
@@ -22,6 +26,6 @@ defmodule BecomicsWeb.LikeController do
 
   defp wildcard_join(name), do: wildcard_join(name, String.length(name))
   defp wildcard_join("", 0), do: "nosuch"
-  defp wildcard_join(name, length) when length < 3, do: "%" <> name
+  defp wildcard_join(name, length) when length < 3, do: name <> "%"
   defp wildcard_join(name, _length), do: "%" <> name <> "%"
 end
