eac1882e12853e44ed7af7df967f52c1838e4e5d
Bengt Kleberg <bengt.kleberg@travelping.com>
2024-02-16 08:38:26 +0100

    Add CI Postgres host

--- config/test.exs
+++ config/test.exs
@@ -8,7 +8,7 @@ import Config
 config :becomics, Becomics.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "localhost",
+  hostname: System.get_env("POSTGRES_HOST") || "localhost",
   database: "becomics_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
